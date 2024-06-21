40fc754f96056dd0de8737fbe96f473cfa879a71
andrewwtpowell <powell.andrew26@gmail.com>
2024-06-21 13:54:08 -0600

    Implemented server side storefile

--- server/server.go
+++ server/server.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	"log"
 	"net"
+	"os"
+	"slices"
 	"sync"
 
 	pb "github.com/andrewwtpowell/dfs/contract"
@@ -81,31 +83,107 @@ func (s *dfsServer) StoreFile(stream pb.DFS_StoreFileServer) error {
     }
 
     // If file is locked return error, otherwise lock the file
-    // defer file unlock
+    if s.lockMap[metadata.GetName()] != metadata.LockOwner &&
+        s.lockMap[metadata.GetName()] != "" {
+        return fmt.Errorf("File %s locked by client %s", metadata.GetName(), s.lockMap[metadata.GetName()])
+    }
+
+    s.lockMap[metadata.GetName()] = metadata.GetLockOwner()
+    defer func() { s.lockMap[metadata.GetName()] = "" }()
 
     // If file already exists on server, has been modified more recently than server verion,
     // and file data does not differ from server version, then update the MetaData for the server version
     // to match the stored file and return
 
-    // Store new/updated file
+    // TODO improve algorithm by sorting the fileList by file name and 
+    // do binary search to find if file is present
+    for _, file := range s.fileList {
 
+        if file.Name == metadata.GetName() &&
+            file.Mtime < metadata.GetMtime() &&
+            file.Crc == metadata.GetCrc() {
+
+            log.Printf("Server file contents match stored file - updating modification time for file %s", metadata.GetName())
+            file.Mtime = metadata.GetMtime()
+
+            return stream.SendAndClose(&pb.MetaData{
+                Name:       file.Name,
+                Size:       file.Size,
+                Mtime:      file.Mtime,
+                Crc:        file.Crc,
+                LockOwner:  "",
+            })
+        }
+
+        if file.Name == metadata.GetName() &&
+            file.Mtime >= metadata.GetMtime() {
+
+            return fmt.Errorf("File already present on server - ignoring")
+        }
+    }
+
+    log.Printf("Opening file %s for writing", metadata.GetName())
+    file, err := os.OpenFile(metadata.GetName(), os.O_WRONLY|os.O_CREATE, 0644)
+    if err != nil {
+        log.Print(err)
+        return err
+    }
+
+    defer func() {
+        if err := file.Close(); err != nil {
+            log.Fatal(err)
+        }
+    }()
+
+    log.Printf("Writing request contents to file %s", metadata.GetName())
     for {
 
         msg, err := stream.Recv()
 
         if err == io.EOF {
             // Reached end of stream, send file metadata response
-            return stream.SendAndClose(&pb.MetaData{})
+            log.Printf("StoreFile successful for file %s", metadata.GetName())
+            info, err := file.Stat()
+            if err != nil {
+                return err
+            }
+            
+            _, err = shared.RefreshFileList(s.mount)
+            if err != nil {
+                log.Print(err)
+                return err
+            }
+
+            var crc uint32
+            for _, file := range s.fileList {
+                if file.GetName() == info.Name() {
+                    crc = file.GetCrc()
+                }
+            }
+            
+            return stream.SendAndClose(&pb.MetaData{
+                Name: info.Name(),
+                Size: uint32(info.Size()),
+                Mtime: uint32(info.ModTime().Unix()),
+                LockOwner: s.lockMap[info.Name()],
+                Crc: crc,
+            })
         }
         if err != nil {
             return err
         }
 
         // Process the request 
+        data := msg.GetFiledata()
+        if data == nil {
+            return fmt.Errorf("Invalid chunk received in file data stream")
+        }
         
-        
-        
-    }
+        bytes, err := file.Write(data.GetContent())
+        if err != nil {
+            return err
+        }
 
-    // Refresh server file list
+        log.Printf("Wrote %d bytes to file %s", bytes, metadata.GetName())
+    }
 }
