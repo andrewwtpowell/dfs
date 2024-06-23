d72fddce7104d61378a0b93b21758f5d024d3a1a
andrewwtpowell <powell.andrew26@gmail.com>
2024-06-23 09:23:11 -0600

    implemented client listfiles and getfilestat

--- client/client.go
+++ client/client.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	pb "github.com/andrewwtpowell/dfs/contract"
 	"github.com/andrewwtpowell/dfs/shared"
@@ -12,9 +14,12 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
+const deadlineTimeout = 7
+
 var (
-    target = flag.String("target", "localhost:50051", "Server target to connect to")
+    server = flag.String("server", "localhost:50051", "Server to connect to in the format ip:port")
     mountPath = flag.String("mount", "mnt/", "Server directory to mount")
+    rpc = flag.String("rpc", "list", "rpc to call (list, store, stat, delete, fetch)")
     fileList []pb.MetaData
     id string
 )
@@ -40,8 +45,8 @@ func main() {
     shared.PrintFileList(&fileList)
 
     // Connect to server
-    log.Printf("connecting to server at %s", *target)
-    conn, err := grpc.NewClient(*target, grpc.WithTransportCredentials(insecure.NewCredentials()))
+    log.Printf("connecting to server at %s", *server)
+    conn, err := grpc.NewClient(*server, grpc.WithTransportCredentials(insecure.NewCredentials()))
     if err != nil {
         log.Fatalf("unable to connect: %s", err)
     }
@@ -50,5 +55,44 @@ func main() {
     //client := pb.NewDFSClient(conn)
 
     // Send request to server
+
+}
+
+// list gets files present on the server and prints them to stdout
+func list(client pb.DFSClient) {
+
+    log.Printf("Requesting list of server-side files")
+    ctx, cancel := context.WithTimeout(context.Background(), deadlineTimeout*time.Second)
+    defer cancel()
+
+    request := pb.MetaData{ Name: "" }
+    list, err := client.ListFiles(ctx, &request)
+    if err != nil {
+        log.Fatalf("client.ListFiles failed: %s", err)
+    }
+
+    for _, file := range list.FileList {
+        log.Printf("%s", file.GetName())
+    }
+}
+
+// stat gets file statistics for a server-side file
+func stat(client pb.DFSClient, filename string) {
+
+    log.Printf("Requesting statistics for file %s", filename)
+    ctx, cancel := context.WithTimeout(context.Background(), deadlineTimeout*time.Second)
+    defer cancel()
+
+    request := pb.MetaData{
+        Name: filename,
+        LockOwner: id,
+    }
+
+    fileStat, err := client.GetFileStat(ctx, &request)
+    if err != nil {
+        log.Fatalf("client.GetFileStat failed: %s", err)
+    }
+
+    log.Printf("%+v", *fileStat)
 
 }
